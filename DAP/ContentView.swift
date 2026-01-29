b9d40d86ccda356666851a00907c6bfc2cf96a85
benniay <101480903+benniay@users.noreply.github.com>
2026-01-28 20:06:35 -0500

    Initial Commit

---
+++ DAP/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DAP
+//
+//  Created by Ben B on 2026-01-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DAP/DAPApp.swift
@@ -0,0 +1,17 @@
+//
+//  DAPApp.swift
+//  DAP
+//
+//  Created by Ben B on 2026-01-28.
+//
+
+import SwiftUI
+
+@main
+struct DAPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
